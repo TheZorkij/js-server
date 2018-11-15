e64f10aa2a978438ca4d8fc61e60942c7683985f
thezorkij <misha_apr@list.ru>
2018-11-15 13:02:49 +0300

    express

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// содежимое index.js
+/*// содежимое index.js
 const http = require('http')
 const port = 3000
 
@@ -10,6 +10,21 @@ const requestHandler = (request, response) => {
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+    })*/
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+    response.send('Hello from Express!')
+})
+
+app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
